3c2d019b1ded18af6d0dafade9e09a5561f87899
Bogomazova Tanya <ip-bogomazova@mail.ru>
2025-10-13 15:40:57 +0500

    add annotation type

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,5 @@
-def filter_by_state(list_state_info: list, state_info: str = "EXECUTED") -> list:
-
-    """ Фильтр словарей по статусу """
+def filter_by_state(list_state_info: list[dict], state_info: str = "EXECUTED") -> list[dict]:
+    """Фильтр словарей по статусу"""
 
     new_list_info = []
     for info in list_state_info:
@@ -9,9 +8,10 @@ def filter_by_state(list_state_info: list, state_info: str = "EXECUTED") -> list
     return new_list_info
 
 
-def sort_by_date(list_state_info: list, sorting_reverse: bool = True) -> list:
+def sort_by_date(list_state_info: list[dict], sorting_reverse: bool = True) -> list[dict]:
+    """Сортирует по дате (по умолчанию на убывание)"""
 
-    """ Сортирует по дате (по умолчанию на убывание)"""
-
-    sort_by_date_list = sorted(list_state_info, key=lambda info: info["date"], reverse=sorting_reverse)
+    sort_by_date_list = sorted(
+        list_state_info, key=lambda info: info["date"], reverse=sorting_reverse
+    )
     return sort_by_date_list
