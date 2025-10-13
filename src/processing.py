58656f7011ef820d9071ac1344ed7f52ea8af82e
Bogomazova Tanya <ip-bogomazova@mail.ru>
2025-10-13 13:03:18 +0500

    add readme

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,11 @@ def filter_by_state(list_state_info: list, state_info = "EXECUTED") -> list:
         if info["state"] == state_info:
             new_list_info.append(info)
     return new_list_info
+
+
+def sort_by_date(list_state_info: list, sorting_reverse: bool = True ) -> list:
+
+    """ Сортирует по дате (по умолчанию на убывание)"""
+
+    sort_by_date_list = sorted(list_state_info, key=lambda info: info["date"], reverse=sorting_reverse)
+    return sort_by_date_list
