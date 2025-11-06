15f635ed78712dca57100a11b6065de9308fcfa6
Bogomazova Tanya <ip-bogomazova@mail.ru>
2025-11-06 19:31:31 +0500

    add modul operations

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+import json
+from typing import Any
+
+def json_read_file(json_file) -> Any:
+    """Преобразует Json файл в список"""
+    try:
+        with open(json_file, "r", encoding="utf-8") as file:
+            try:
+                operations = json.load(file)
+            except json.decoder.JSONDecodeError:
+                operations = []
+    except FileNotFoundError:
+        operations = []
+    return operations
