57a768b1b534a35c92e15bf3b3d9bd93d14fd893
Bogomazova Tanya <ip-bogomazova@mail.ru>
2025-11-15 18:48:31 +0500

    homework13.1

---
+++ src/read_csv_excel.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def read_csv_file(csv_file) -> list[dict]:
+    """Преобразует файл csv в список словарей"""
+    csv_file = pd.read_csv(csv_file, sep=";")
+    csv_file_list = csv_file.to_dict("records")
+    return csv_file_list
+
+
+def read_excel(xlsx_file) -> list[dict]:
+    """Преобразует файл xlsx в список словарей"""
+    excel_file = pd.read_excel(xlsx_file)
+    excel_file_list = excel_file.to_dict("records")
+    return excel_file_list
